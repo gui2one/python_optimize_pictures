fee75f45593c8dce080fb8c0ffdaf157836e35fb
gui2one <guillaume.rouault.fx@gmail.com>
2022-03-08 22:32:58 +0100

    at least it works ...

--- src/Application.py
+++ src/Application.py
@@ -1,18 +1,23 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QFileDialog, QVBoxLayout, QListView
-from PyQt5.QtGui import QIcon
-
+from fileinput import filename
+from PyQt5.QtWidgets import QWidget, QProgressBar, QPushButton, QFileDialog, QVBoxLayout, QHBoxLayout, QListWidget, QListWidgetItem
+from PyQt5.QtGui import  QStandardItem
+from PyQt5.QtCore import Qt
+from picture_converter import listFilesInDir, optimizePicture
 class Converter(QWidget) :
 
     pictureDir = ""
     label = None
     btn_dir = None
+    btn_optimize = None
     list_view = None
+    list_view_model = None
+    progress = None
     def __init__(self, name):
         super().__init__()
 
         self.initUI()
 
-        self.pictureDir = ""
+        # self.pictureDir = ""
 
     def initUI(self):
         self.setGeometry(200, 200, 800, 600)
@@ -20,17 +25,78 @@ class Converter(QWidget) :
         # self.setWindowIcon(QIcon('web.png'))
 
         layout = QVBoxLayout(self)
+        layout.setAlignment(Qt.AlignTop)
         self.btn_dir = QPushButton("Choisir un Dossier", self)
         layout.addWidget(self.btn_dir)
         self.btn_dir.clicked.connect(self.getFolderName)
 
-        self.list_view = QListView()
-        layout.addWidget(self.list_view)
+        sources_layout = QHBoxLayout()
+        layout.addLayout(sources_layout)
+        btns_layout = QVBoxLayout()
+        btns_layout.setAlignment(Qt.AlignTop)
+        sources_layout.addLayout(btns_layout)
+        select_all_btn = QPushButton("Select all")
+        select_all_btn.clicked.connect(self.selectAll)
+        btns_layout.addWidget(select_all_btn)
+        deselect_all_btn = QPushButton("Deselect all")
+        deselect_all_btn.clicked.connect(self.deselectAll)
+        btns_layout.addWidget(deselect_all_btn)
+        
+        self.list_view = QListWidget()
+        self.list_view.setMaximumHeight(250)
+
+        sources_layout.addWidget(self.list_view)
+
+
+        self.btn_optimize = QPushButton("Optimize Pictures", self)
+        self.btn_optimize.clicked.connect(self.optimizePictures)
+        layout.addWidget(self.btn_optimize)
+
+        self.progress = QProgressBar()
+        layout.addWidget(self.progress)
         
         self.show()
 
     def getFolderName(self): 
         selected_folder = str(QFileDialog.getExistingDirectory(None, "Select Directory"))
+        if selected_folder.strip(" ") == '' : return
         self.pictureDir = selected_folder
-        print(self.pictureDir)
+        # print(self.pictureDir)
         self.btn_dir.setText(self.pictureDir)
+        files = listFilesInDir(self.pictureDir)
+
+        self.list_view.clear()
+        for file_name in files :
+
+            item = QListWidgetItem()
+            item.setText(file_name)
+            item.setCheckState(Qt.Checked)            
+
+            self.list_view.addItem(item)
+
+    def optimizePictures(self):
+
+        num_pictures = self.list_view.count()
+        for i in range(num_pictures):
+            
+            item = self.list_view.item(i)
+            # print(item.getCheckState())
+            # print(item.checkState())
+            if item.checkState() :
+                print(item.text())
+                optimizePicture(item.text(), self.pictureDir)
+
+            percent = int(i / (num_pictures-1) * 100)
+            self.progress.setValue(percent)
+
+    def selectAll(self):
+        for i in range(self.list_view.count()):
+            
+            item = self.list_view.item(i)
+            item.setCheckState(Qt.Checked)
+
+    def deselectAll(self):
+        for i in range(self.list_view.count()):
+            
+            item = self.list_view.item(i)
+            item.setCheckState(Qt.Unchecked)
---
+++ src/picture_converter.py
@@ -0,0 +1,60 @@
+from PIL import Image
+import os
+
+def checkImageExtension(file_name) :
+    extension_list = [".webp",".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".tif", ".gif"]
+    root , ext = os.path.splitext(file_name)
+    if ext.lower() in extension_list:
+        return True
+
+    return False
+
+def listFilesInDir( dir_path):
+    files = os.listdir(os.path.join(dir_path))
+    
+    clean_files = []
+    for item in files :
+        if not os.path.isdir(os.path.join(dir_path, item)):
+            if checkImageExtension(item):
+                clean_files.append(item)
+    return clean_files
+
+def optimizePicture(file_name, pictures_folder):
+    with Image.open(os.path.join(pictures_folder, file_name), 'r') as img :
+
+
+        width = img.size[0]
+        height = img.size[1]
+
+        scale_ratio = 1.0
+        limit  = 256
+        max_dim = max(width, height)
+        if  max_dim > limit :
+            scale_ratio = limit / max_dim
+        new_width =  int(width * scale_ratio)
+        new_height =  int(height * scale_ratio)
+        # num_components = img.info()
+        
+        print("\tmode : %s"%(img.mode))
+        print("\tformat : %s"%(img.format_description))
+        print("\tsize : %d %d"%(width, height))
+        print("\tNew size : %d %d"%(new_width, new_height))
+
+        root, ext = os.path.splitext(file_name)
+        img.load()
+        img = img.resize(size=(new_width, new_height))
+        # img.show()
+
+        optim_folder = os.path.join(pictures_folder, "OPTIM")
+        if not os.path.isdir(optim_folder):
+            os.makedirs(optim_folder, mode=0o700)
+        save_path = os.path.join(optim_folder,root+"_OPTIM"+ext)
+        
+        if ext.lower() == ".jpg" or ext.lower() == ".jpeg":
+            img.save(save_path, quality=50)
+        else :
+            img.save(save_path)
+
+        print("\tSaving to : "+os.path.abspath(save_path))
+
+        img.close()
