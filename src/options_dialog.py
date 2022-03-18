a9f12093e1a4bd28a2c27c234a5a8d38aeabf9dc
gui2one <guillaume.rouault.fx@gmail.com>
2022-03-18 17:46:54 +0100

    added ColorPickerWidget

--- src/options_dialog.py
+++ src/options_dialog.py
@@ -1,16 +1,17 @@
+from turtle import color
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 from widgets.checkbox_widget import CheckBoxWidget
+from widgets.color_picker import ColorPickerWidget
 
 class OptionsDialog(QDialog) :
 
-    def __init__(self, parent = None) -> None:
+    def __init__(self, parent = None):
         super(OptionsDialog, self).__init__(parent)
         self.setWindowTitle("Options")
-        self.setFixedSize(600,650)
-        
-        # self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.setFixedSize(500,500)
         self.setWindowModality(Qt.ApplicationModal)
         
         self.initUI()
@@ -21,4 +22,11 @@ class OptionsDialog(QDialog) :
 
         option1 = CheckBoxWidget("Convert PNGs to JPEG", self)
         layout.addWidget(option1)
+
+
+        color_option = ColorPickerWidget(self)
+        # color_option.update()
+
+        layout.addWidget(color_option)
+
         self.setLayout(layout)
---
+++ src/widgets/color_picker.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class ColorPickerWidget(QWidget):
+    
+
+    clicked = pyqtSignal()
+    def __init__(self, parent = None):
+        super(ColorPickerWidget, self).__init__(parent)
+        # print("ColorPickerWidget")
+        self.setFixedSize(50,25)
+        self.color = QColor("white")
+        
+    def paintEvent(self, event):
+        print(event)
+        painter = QPainter(self)
+
+        painter.setPen(QPen(Qt.green,  8, Qt.DashLine))
+        painter.fillRect(event.rect(), self.color)
+
+    def mousePressEvent(self, ev):
+        w = QColorDialog(self)
+        # w.exec()
+
+        _color = w.getColor()
+        self.color = _color
+        self.clicked.emit()
+
+    def value(self) -> QColor : 
+        return self.color
